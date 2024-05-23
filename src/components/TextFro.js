e36db4e37c9b2686ba68cf799a1c82aa9dbb4636
Dina <dinanath88090@outlook.com>
2024-05-23 10:31:11 +0530

    practice 2

--- src/components/TextFro.js
+++ src/components/TextFro.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react'
 
-export default function TextFro() {
-const handleUpClick=()=>{
-  // console.log("You Clicked handleUpClick");
-  let newtext=text.toUpperCase();
-  setText(newtext);
+export default function TextFro(props) {
+
+const handleOnClick=()=>{
+  console.log("Cliked on handleOnClick"+text);
+  let newText=text.toUpperCase();
+  setText(newText);
 }
 
 const handleOnChange=(event)=>{
-  // console.log("On Change");
-  setText(event.target.value)
+  console.log("Clicked on Changeed");
+  setText(event.target.value);
 }
 
-const[text,setText]=useState('Enter Text Here');
+  const [text,setText]=useState('Enter Your textg hear');
   return (
     <div>
-      <div className="mb-3">
+      <h1>{props.heading}</h1>
+       <div className="mb-3">
         <label htmlFor="myBox" className="form-label">Some</label>
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick} >Cunvert to uppercase</button>
+        <button className="btn btn-primary" onClick={handleOnClick}>Cunvert to uppercase</button>
     </div>
   )
 }
